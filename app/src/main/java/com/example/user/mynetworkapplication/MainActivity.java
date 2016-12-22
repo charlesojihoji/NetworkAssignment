b3501c4dee37e399dfdd6b57598354d22eff6b06
Charles Oji <charlesojihoji@gmail.com>
2016-12-22 18:00:41 -0500

    first commit

---
+++ app/src/main/java/com/example/user/mynetworkapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.user.mynetworkapplication;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public void doMagic(View view) {
+        if (isNetworkAvailable())
+            System.out.println("We have internet access!");
+        else
+            System.out.println("We don't have internet access!");
+    }
+}
